4c133800adba1a87e04bdd398716ba2333b5e452
zacbayhan <zacbayhan@gmail.com>
2016-04-10 23:28:35 -0400

    Added files via upload

---
+++ optionsPricing.cpp
@@ -0,0 +1,32 @@
+/*
+
+
+
+
+*/
+
+#include <iostream>
+#include <vector>
+#include"math.h"
+
+using namespace std;
+
+
+double options();
+
+
+int main(){
+
+	cout << "yea bitches!\n";
+	
+	options();
+	return 1;
+}
+
+
+
+double options (){
+	cout << "in options!\n";
+	
+}
+
